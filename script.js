4d7bf0f6ac816c71649a3f1cf786bf1804079768
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-10 00:05:41 -0500

    grabbed all relavant buttons

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const allButtons = document.querySelectorAll("button");
 const clearButton = document.querySelectorAll(`button[id="1.01"]`);
 const firstRowColorButtons = document.querySelectorAll(`button[id^="1."]:not([id="1.01"])`);
 const firstColumnColorButtons = document.querySelectorAll(`button[id$=".01"]:not([id="1.01"])`);
+const changeableButtons = document.querySelectorAll(`button:not([id^="1."]):not([id$=".01])`);
 
 clearButton.forEach((button) => {
   button.addEventListener("click", () => {
@@ -18,5 +19,11 @@ firstRowColorButtons.forEach((button) => {
 firstColumnColorButtons.forEach((button) => {
   button.addEventListener("click", () => {
     console.log(`Button ${button.id} clicked!`);
+  });
+});
+
+changeableButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(`blank button clicked!`);
   });
 });
