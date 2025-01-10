07ffeb306452dab01c2bea3b385b6ce8eafd7d78
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-10 00:01:12 -0500

    grabbed all color and clear buttons

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const allButtons = document.querySelectorAll("button");
-
+const clearButton = document.querySelectorAll(`button[id="1.01"]`);
 const firstRowColorButtons = document.querySelectorAll(`button[id^="1."]:not([id="1.01"])`);
+const firstColumnColorButtons = document.querySelectorAll(`button[id$=".01"]:not([id="1.01"])`);
+
+clearButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(`Clear button clicked!`);
+  });
+});
 
 firstRowColorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(`Button ${button.id} clicked!`);
+  });
+});
+
+firstColumnColorButtons.forEach((button) => {
   button.addEventListener("click", () => {
     console.log(`Button ${button.id} clicked!`);
   });
