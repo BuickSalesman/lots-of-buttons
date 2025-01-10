988ccd0b5a401f3b05437597782ebc9a67c33e40
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-10 00:53:00 -0500

    can change color of columns

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
-const allButtons = document.querySelectorAll("button");
 const clearButton = document.querySelectorAll(`button[id="1.01"]`);
 const firstRowColorButtons = document.querySelectorAll(`button[id^="1."]:not([id="1.01"])`);
 const firstColumnColorButtons = document.querySelectorAll(`button[id$=".01"]:not([id="1.01"])`);
-const changeableButtons = document.querySelectorAll(`button:not([id^="1."]):not([id$=".01])`);
+const changeableButtons = document.querySelectorAll(`button:not([id^="1."]):not([id$=".01"])`);
 
 clearButton.forEach((button) => {
   button.addEventListener("click", () => {
-    console.log(`Clear button clicked!`);
+    clear(button);
   });
 });
 
 firstRowColorButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    console.log(`Button ${button.id} clicked!`);
+    changeColumnColor(button);
   });
 });
 
 firstColumnColorButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    console.log(`Button ${button.id} clicked!`);
+    changeRowColor(button);
   });
 });
 
 changeableButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    console.log(`blank button clicked!`);
+    console.log(`blank button ${button.id} clicked!`);
   });
 });
+
+function clear(button) {
+  console.log("cleared!");
+  changeableButtons.forEach((button) => {
+    button.style.backgroundColor = "rgb(255, 255, 255)";
+  });
+}
+
+function changeColumnColor(button) {
+  console.log(`column ${button.id} change!`);
+  let buttonSuffix = button.id.slice(-3);
+  let escapedSuffix = buttonSuffix.replace(".", "\\.");
+
+  let columnButtons = Array.from(document.querySelectorAll(`button[id$=${escapedSuffix}]`));
+  columnButtons = columnButtons.filter((colButton) => colButton !== button);
+  columnButtons.forEach((button) => {
+    button.style.backgroundColor = "rgb(0,0,0)";
+  });
+}
+
+function changeRowColor(button) {
+  console.log(`row ${button.id} change!`);
+}
